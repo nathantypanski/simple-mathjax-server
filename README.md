2a1eafbeacff440c58f6f50e396c96a5ab0fa215
Nathan Typanski <ntypanski@gmail.com>
2014-12-14 16:08:10 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# mathjax-server
+
+This is an extremely simple Happstack server for acting as a local MathJax
+"CDN" for my (local)
+[Gitit wiki](http://nathantypanski.com/blog/2014-07-09-personal-wiki.html).
---
+++ stack.hs
@@ -0,0 +1,10 @@
+module Main where
+
+import Happstack.Server ( Browsing(EnableBrowsing), nullConf
+                        , serveDirectory, simpleHTTP, Conf(port)
+                        )
+
+conf = nullConf { port = 12019 }
+
+main :: IO ()
+main = simpleHTTP nullConf $ serveDirectory EnableBrowsing [] "."
